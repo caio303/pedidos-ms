47c5d8d17f8a63eaca349d48d3867e5f91b980a3
caio303 <caioalves_diogo@hotmail.com>
2025-02-14 00:13:57 -0300

    feat: Removendo topicos unused

--- src/main/java/com/postech/gerencie/pedidos/config/PedidoSeviceProperties.java
+++ src/main/java/com/postech/gerencie/pedidos/config/PedidoSeviceProperties.java
@@ -9,29 +9,8 @@ public class PedidoSeviceProperties {
     @Value("${queue.topics.novo-pedido.name}")
     private String novoPedidoChannelName;
 
-    @Value("${queue.topics.estoque-insuficiente.name}")
-    private String estoqueInsuficienteChannelName;
-
-    @Value("${queue.topics.entrega-iniciada-topic.name}")
-    private String entregaIniciadaChannelName;
-
-    @Value("${queue.topics.entrega-concluida.name}")
-    private String entregaConcluidaChannelName;
-
-
     public String getNovoPedidoChannelName() {
         return novoPedidoChannelName;
     }
 
-    public String getEstoqueInsuficienteChannelName() {
-        return estoqueInsuficienteChannelName;
-    }
-
-    public String getEntregaIniciadaChannelName() {
-        return entregaIniciadaChannelName;
-    }
-
-    public String getEntregaConcluidaChannelName() {
-        return entregaConcluidaChannelName;
-    }
 }

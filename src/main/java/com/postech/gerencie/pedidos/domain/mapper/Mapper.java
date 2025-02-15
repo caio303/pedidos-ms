9812e87264c825290e67eafd13b396655ac6258d
caio303 <caioalves_diogo@hotmail.com>
2025-02-15 12:17:26 -0300

    feat: Remover dependencia Mapper

--- src/main/java/com/postech/gerencie/pedidos/domain/mapper/Mapper.java
+++
@@ -1,6 +0,0 @@
-package com.postech.gerencie.pedidos.domain.mapper;
-
-public interface Mapper {
-    <D> D map(Object source, Class<D> destinationType);
-    void map(Object source, Object destination);
-}
--- src/main/java/com/postech/gerencie/pedidos/gateway/queue/dispatcher/NovoPedidoStreamDispatcher.java
+++ src/main/java/com/postech/gerencie/pedidos/gateway/queue/dispatcher/NovoPedidoStreamDispatcher.java
@@ -1,8 +1,6 @@
 package com.postech.gerencie.pedidos.gateway.queue.dispatcher;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.postech.gerencie.pedidos.config.PedidoSeviceProperties;
-import com.postech.gerencie.pedidos.domain.mapper.Mapper;
 import com.postech.gerencie.pedidos.gateway.queue.messages.NovoPedidoMensagem;
 import com.postech.gerencie.pedidos.usecase.dto.PedidoDTO;
 import org.slf4j.Logger;
@@ -16,14 +14,10 @@ public class NovoPedidoStreamDispatcher implements NovoPedidoDispatcher {
 
     private final StreamBridge streamBridge;
     private final PedidoSeviceProperties pedidoSeviceProperties;
-    private final Mapper mapper;
-    private final ObjectMapper objectMapper;
 
-    public NovoPedidoStreamDispatcher(StreamBridge streamBridge, PedidoSeviceProperties pedidoSeviceProperties, Mapper mapper, ObjectMapper objectMapper) {
+    public NovoPedidoStreamDispatcher(StreamBridge streamBridge, PedidoSeviceProperties pedidoSeviceProperties) {
         this.streamBridge = streamBridge;
         this.pedidoSeviceProperties = pedidoSeviceProperties;
-        this.mapper = mapper;
-        this.objectMapper = objectMapper;
     }
 
     @Override
